1f3bef82b21e74f8248b40bd4d4d0df5d5e23cc2
RDjarbeng <richarddjarbeng@gmail.com>
2023-07-28 10:22:45 +0200

    make APN and url variables passed to function

--- sendGetRequest.ino
+++ sendGetRequest.ino
@@ -8,8 +8,10 @@ void setup() {
   Serial.begin(baudRate); // Serial monitor
   gprsShield.begin(baudRate); // gprsShield module
   delay(1000);
-  sendGetRequest();
-  
+  String apn = "internet.mtn"; // Change this to the desired APN of cell provider
+  String url = "http://google.com/"; // Change this to the desired URL
+
+  sendGetRequestWithParameters(apn, url);  
 
   // Add any additional code here if needed
 }
@@ -17,6 +19,38 @@ void setup() {
 void loop() {
   // Add your main program logic here if needed
 }
+
+/**
+ * The sequence of commands establishes the GPRS connection for performing an HTTP GET request
+ * @param apn The Access Point Name for the SIM card's cellular network.
+ * @param url The URL of the server you want to reach.
+ */
+void sendGetRequestWithParameters(const String& apn, const String& url) {
+  sendCommand("AT"); //expected value OK
+  sendCommand("AT+CIPSHUT"); //expected value OK
+  sendCommand("AT+SAPBR=0,1"); //expected value OK
+  sendCommand("AT+SAPBR=3,1,\"Contype\",\"GPRS\""); // open GPRS context establish GPRS connection
+  
+  // Change the APN value for the SIM card based on the provided parameter
+  String sapbr_apn_command = "AT+SAPBR=3,1,\"APN\",\"" + apn + "\"";
+  sendCommand(sapbr_apn_command.c_str());
+  
+  sendCommand("AT+SAPBR=1,1"); // open GPRS context bearer
+  sendCommand("AT+HTTPINIT"); // initiate HTTP request
+  sendCommand("AT+HTTPPARA=\"CID\",1"); // set parameters for http session
+  
+  // Change the URL to the provided parameter
+  String http_url_command = "AT+HTTPPARA=\"URL\",\"" + url + "\"";
+  sendCommand(http_url_command.c_str());
+  
+  sendCommand("AT+HTTPACTION=0"); // send http request to specified URL, GET session start
+  delay(9000); // wait for response for 9 seconds, reduce or increase based on your need
+  sendCommand("AT+HTTPREAD"); // read results of request, normally contains status code 200 if successful
+  sendCommand("AT+HTTPTERM"); // close http connection
+  sendCommand("AT+CIPSHUT"); // close or turn off network connection
+  sendCommand("AT+SAPBR=0,1"); // close GPRS context bearer
+}
+
 /**
  * The sequence of commands establishes the GPRS connection for performing an HTTP request
 */
